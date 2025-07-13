a410ccc97e70b295b08b654babbada5709284d4a
devidardhruvil-codes <dsilento01@gmail.com>
2025-07-13 12:27:27 +0530

    Auth Screens UI completed

---
+++ lib/appwrite.ts
@@ -0,0 +1,7 @@
+export const appwriteConfig = {
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  platform: "com.dhruvil.fastfood",
+  databaseId: "6873487b0026767ce7ef",
+  userCollectionId: "687348c40008f800f76c",
+};
