0609dbddfae157846c67c4e40a42aebbf3710c5e
devidardhruvil-codes <dsilento01@gmail.com>
2025-07-13 16:22:36 +0530

    Auth functionalities added

--- app/(auth)/sign-up.tsx
+++ app/(auth)/sign-up.tsx
@@ -1,5 +1,6 @@
 import CustomButton from "@/components/CustomButton";
 import CustomInput from "@/components/CustomInput";
+import { createUser } from "@/lib/appwrite";
 import { Link, router } from "expo-router";
 import React, { useState } from "react";
 import { Alert, Text, View } from "react-native";
@@ -9,7 +10,9 @@ const signup = () => {
   const [form, setForm] = useState({ name: "", email: "", password: "" });
 
   const submit = async () => {
-    if (!form.name || !form.email || !form.password)
+    const { email, password, name } = form;
+
+    if (!name || !email || !password)
       return Alert.alert(
         "Error",
         "Please enter valid email address & password."
@@ -17,9 +20,7 @@ const signup = () => {
     setIsSubmitting(true);
 
     try {
-      //Call Appwrite Sign Up function
-
-      Alert.alert("Success", "User signed up successfully");
+      await createUser({ email, password, name });
       router.replace("/");
     } catch (error: any) {
       Alert.alert("Error", error.message);
--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -1,7 +1,89 @@
+import { CreateUserParams, SignInParams } from "@/type";
+import {
+  Account,
+  Avatars,
+  Client,
+  Databases,
+  ID,
+  Query,
+} from "react-native-appwrite";
+
 export const appwriteConfig = {
-  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT,
-  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID,
+  endpoint: process.env.EXPO_PUBLIC_APPWRITE_ENDPOINT!,
+  projectId: process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!,
   platform: "com.dhruvil.fastfood",
   databaseId: "6873487b0026767ce7ef",
   userCollectionId: "687348c40008f800f76c",
+};
+
+// APPWRITE CLIENT SETUP
+export const client = new Client();
+client
+  .setEndpoint(appwriteConfig.endpoint)
+  .setProject(appwriteConfig.projectId)
+  .setPlatform(appwriteConfig.platform);
+
+// APPWRITE FUNCTIONALITIES SETUP
+export const account = new Account(client);
+export const databases = new Databases(client);
+const avatars = new Avatars(client);
+
+// CREATE USER FUCNTION FOR SIGNUP
+export const createUser = async ({
+  email,
+  password,
+  name,
+}: CreateUserParams) => {
+  try {
+    const newAccount = await account.create(ID.unique(), email, password, name);
+    if (!newAccount) throw Error;
+
+    await signIn({ email, password });
+
+    const avatarUrl = avatars.getInitialsURL(name);
+
+    //STORING THE NEW USER INTO THE DATABASE
+    return await databases.createDocument(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      ID.unique(),
+      {
+        name,
+        email,
+        accountId: newAccount.$id,
+        avatar: avatarUrl,
+      }
+    );
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+// SIGNIN FUNCTION
+export const signIn = async ({ email, password }: SignInParams) => {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+  } catch (error) {
+    throw new Error(error as string);
+  }
+};
+
+// GET CURRENT USER
+export const getCurrentUser = async () => {
+  try {
+    const currentAccount = await account.get();
+    if (!currentAccount) throw Error;
+
+    const currentUser = await databases.listDocuments(
+      appwriteConfig.databaseId,
+      appwriteConfig.userCollectionId,
+      [Query.equal("accountId", currentAccount.$id)]
+    );
+
+    if (!currentUser) throw Error;
+
+    return currentUser.documents[0];
+  } catch (error) {
+    throw new Error(error as string);
+  }
 };
